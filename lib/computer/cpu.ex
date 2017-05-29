ed0f2efde377412f0589e71a8393a34b92ca5916
Kevin Kotowski <kevink@8thlight.com>
2017-05-29 15:18:43 -0500

    added cpu module

---
+++ lib/computer/cpu.ex
@@ -0,0 +1,39 @@
+defmodule Computer.Cpu do
+  alias __MODULE__
+  alias Computer.Command
+
+  defstruct [:command]
+
+  @type t :: %Cpu{command: Command}
+
+  def new() do
+    {:ok, command} = Command.new()
+    %Cpu{command: command}
+  end
+
+  @spec fetch(t, Command) :: {:ok, t} | {:busy, integer}
+  def fetch(cpu, command) do
+    case idle?(cpu) do
+      true -> {:ok, %Cpu{command: command}}
+      false -> {:busy, Command.duration(cpu.command)}
+    end
+  end
+
+  @spec tick(t) :: {Command.unit, t}
+  def tick(cpu) do
+    case cpu.command do
+      %{wait: 0, process: 0} ->
+        {:idle, cpu}
+      %{wait: 0, process: x} when x > 0 ->
+        command = Map.merge(cpu.command, %{process: x - 1})
+        {:process, %Cpu{command: command}}
+      %{wait: x, process: _} when x > 0 ->
+        command = Map.merge(cpu.command, %{wait: x - 1})
+        {:wait, %Cpu{command: command}}
+    end
+  end
+
+  def idle?(cpu) do
+    Command.duration(cpu.command) < 1
+  end
+end
